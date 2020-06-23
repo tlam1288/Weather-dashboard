488c872f9319e0b881c72ad9f0d54ab62a96841a
nhuanthi <tlam1288@gmail.com>
2020-06-22 19:58:47 -0700

    added functions to find UV index and display the 5 day forecast for each day. When city is searched it is added to saved list

--- Assets/script.js
+++ Assets/script.js
@@ -9,5 +9,76 @@ $("button").on("click", function () {
     method: "GET",
   }).then(function (response) {
     console.log(response);
+
+    var newSearchP = $("<p>").text(searchCity);
+    $("#searchForm").append(newSearchP);
+
+    var name = response.name;
+    var nameElem = $("<h2>").text(name);
+
+    var temperature = response.main.temp;
+    var fConvert = ((temperature - 273.15) * 1.8 + 32).toFixed(2);
+    var tempElem = $("<p>").text("Temperature: " + fConvert);
+
+    var humidity = response.main.humidity;
+    var humidityElem = $("<p>").text("Humidity: " + humidity);
+
+    var windSpeed = response.wind.speed;
+    var windElem = $("<p>").text("Wind Speed: " + windSpeed);
+
+    $("#currentWeather").empty();
+    $("#currentWeather").append(nameElem, tempElem, humidityElem, windElem);
+
+    //get lat and long to find UV index
+    var longitude = "&lon=" + response.coord.lon;
+    var latitude = "&lat=" + response.coord.lat;
+    var uvURL =
+      "http://api.openweathermap.org/data/2.5/uvi?" +
+      latitude +
+      longitude +
+      apiKey;
+
+    $.ajax({
+      url: uvURL,
+      method: "GET",
+    }).then(function (uvindex) {
+      console.log(uvindex);
+      var uvIndex = uvindex.value;
+      var uvElem = $("<p>").text("UV Index: " + uvIndex);
+      $("#currentWeather").append(uvElem);
+    });
+  });
+
+  //5 day forecast
+  var forecastURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + apiKey;
+
+  $.ajax({
+    url: forecastURL,
+    method: "GET",
+  }).then(function (forecast) {
+    console.log(forecast);
+    var forecasts = forecast.list;
+
+    for (var i = 0; i < forecasts.length; i++) {
+      var forecastDate = forecasts[i].dt_txt;
+      var forecastCondition = forecasts[i].weather[0].main;
+      var forecastTemp = forecasts[i].main.temp;
+      var fForecastConvert = ((forecastTemp - 273.15) * 1.8 + 32).toFixed(2);
+      var forecastHumidity = forecasts[i].main.humidity;
+
+      //displays each day separately
+      if ([i] % 8 === 0) {
+        console.log(forecastDate);
+        console.log(forecastCondition);
+        console.log(fForecastConvert);
+        console.log(forecastHumidity);
+      }
+
+      //console.log(fForecastConvert);
+
+      var newDiv = $("<div>").append(forecastDate);
+      $("#5dayForecast").append(newDiv);
+    } // closes for loop
   });
 });

d12c7433e3fa823be297331adcd84c139f5af2e8
nhuanthi <tlam1288@gmail.com>
2020-09-03 12:14:57 -0700

    changed all var to let/const

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,9 @@
-var storage = [];
+let storage = [];
 
 function getWeather() {
-  var apiKey = "&appid=3c85c1039b089f52f3c466c42a12368f";
-  var searchCity = $("#searchCity").val();
-  var queryURL =
+  let apiKey = "&appid=3c85c1039b089f52f3c466c42a12368f";
+  let searchCity = $("#searchCity").val();
+  let queryURL =
     "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=" +
     searchCity +
     apiKey;
@@ -12,32 +12,32 @@ function getWeather() {
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    var newSearchP = $("<div>")
+    let newSearchP = $("<div>")
       .attr("class", "card p-2 searchNumber")
       .text(searchCity);
     $("#searchForm").append(newSearchP);
 
     //current city weather
-    var name = response.name;
-    var nameElem = $("<h2>").text(name);
+    let name = response.name;
+    let nameElem = $("<h2>").text(name);
 
-    var temperature = response.main.temp;
-    var tempElem = $("<p>").text("Temperature: " + temperature + "F");
+    let temperature = response.main.temp;
+    let tempElem = $("<p>").text("Temperature: " + temperature + "F");
 
-    var humidity = response.main.humidity;
-    var humidityElem = $("<p>").text("Humidity: " + humidity + "%");
+    let humidity = response.main.humidity;
+    let humidityElem = $("<p>").text("Humidity: " + humidity + "%");
 
-    var windSpeed = response.wind.speed;
-    var windElem = $("<p>").text("Wind Speed: " + windSpeed + "mph");
+    let windSpeed = response.wind.speed;
+    let windElem = $("<p>").text("Wind Speed: " + windSpeed + "mph");
 
     $("#currentWeather").empty();
     $("#currentWeather").append(nameElem, tempElem, humidityElem, windElem);
 
     //ends currentCIty funtion
     //get lat and long to find UV index
-    var longitude = "&lon=" + response.coord.lon;
-    var latitude = "&lat=" + response.coord.lat;
-    var uvURL =
+    let longitude = "&lon=" + response.coord.lon;
+    let latitude = "&lat=" + response.coord.lat;
+    let uvURL =
       "https://api.openweathermap.org/data/2.5/uvi?&units=imperial" +
       latitude +
       longitude +
@@ -47,8 +47,8 @@ function getWeather() {
       url: uvURL,
       method: "GET",
     }).then(function (uvindex) {
-      var uvIndex = parseInt(uvindex.value);
-      var uvElem = $("<p>").html("UV Index:  <span>" + uvIndex + "</span>");
+      let uvIndex = parseInt(uvindex.value);
+      let uvElem = $("<p>").html("UV Index:  <span>" + uvIndex + "</span>");
 
       $("#currentWeather").append(uvElem);
 
@@ -63,7 +63,7 @@ function getWeather() {
   });
 
   //5 day forecast
-  var forecastURL =
+  let forecastURL =
     "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&q=" +
     searchCity +
     apiKey;
@@ -72,24 +72,24 @@ function getWeather() {
     url: forecastURL,
     method: "GET",
   }).then(function (forecast) {
-    var forecasts = forecast.list;
+    let forecasts = forecast.list;
 
     $("#5dayForecast").empty();
-    for (var i = 0; i < forecasts.length; i++) {
-      var forecastDate = $("<p>").text(
+    for (let i = 0; i < forecasts.length; i++) {
+      let forecastDate = $("<p>").text(
         forecasts[i].dt_txt.replace("00:00:00", " ")
       );
-      var forecastCondition = $("<p>").text(forecasts[i].weather[0].main);
-      var forecastTemp = $("<p>").text(
+      let forecastCondition = $("<p>").text(forecasts[i].weather[0].main);
+      let forecastTemp = $("<p>").text(
         "Temp: " + forecasts[i].main.temp + " F"
       );
-      var forecastHumidity = $("<p>").text(
+      let forecastHumidity = $("<p>").text(
         "Humidity: " + forecasts[i].main.humidity + "%"
       );
 
       //displays each day separately
       if ([i] % 8 === 0) {
-        var newDiv = $("<div>").attr("class", "col card mr-3 forecast");
+        let newDiv = $("<div>").attr("class", "col card mr-3 forecast");
         newDiv.append(
           forecastDate,
           forecastCondition,
@@ -109,16 +109,16 @@ function getWeather() {
 }
 
 //rendering from local storage
-var apiKey = "&appid=3c85c1039b089f52f3c466c42a12368f";
-var savedSearch = JSON.parse(localStorage.getItem("city"));
+let apiKey = "&appid=3c85c1039b089f52f3c466c42a12368f";
+let savedSearch = JSON.parse(localStorage.getItem("city"));
 if (savedSearch === null) {
   savedSearch = " ";
 } else {
   savedSearch = JSON.parse(localStorage.getItem("city"));
 }
-var renderSavedSearch = savedSearch[savedSearch.length - 1];
+let renderSavedSearch = savedSearch[savedSearch.length - 1];
 
-var queryURL2 =
+let queryURL2 =
   "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=" +
   renderSavedSearch +
   apiKey;
@@ -127,24 +127,24 @@ $.ajax({
   url: queryURL2,
   method: "GET",
 }).then(function (response) {
-  var name = response.name;
-  var nameElem = $("<h2>").text(name);
+  let name = response.name;
+  let nameElem = $("<h2>").text(name);
 
-  var temperature = response.main.temp;
-  var tempElem = $("<p>").text("Temperature: " + temperature);
+  let temperature = response.main.temp;
+  let tempElem = $("<p>").text("Temperature: " + temperature);
 
-  var humidity = response.main.humidity;
-  var humidityElem = $("<p>").text("Humidity: " + humidity);
+  let humidity = response.main.humidity;
+  let humidityElem = $("<p>").text("Humidity: " + humidity);
 
-  var windSpeed = response.wind.speed;
-  var windElem = $("<p>").text("Wind Speed: " + windSpeed);
+  let windSpeed = response.wind.speed;
+  let windElem = $("<p>").text("Wind Speed: " + windSpeed);
 
   $("#currentWeather").empty();
   $("#currentWeather").append(nameElem, tempElem, humidityElem, windElem);
 
-  var longitude = "&lon=" + response.coord.lon;
-  var latitude = "&lat=" + response.coord.lat;
-  var uvURL =
+  let longitude = "&lon=" + response.coord.lon;
+  let latitude = "&lat=" + response.coord.lat;
+  let uvURL =
     "https://api.openweathermap.org/data/2.5/uvi?&units=imperial" +
     latitude +
     longitude +
@@ -154,8 +154,8 @@ $.ajax({
     url: uvURL,
     method: "GET",
   }).then(function (uvindex) {
-    var uvIndex = parseInt(uvindex.value);
-    var uvElem = $("<p>").html("UV Index:  <span>" + uvIndex + "</span>");
+    let uvIndex = parseInt(uvindex.value);
+    let uvElem = $("<p>").html("UV Index:  <span>" + uvIndex + "</span>");
 
     $("#currentWeather").append(uvElem);
 
@@ -168,7 +168,7 @@ $.ajax({
     }
   });
 
-  var forecastURL =
+  let forecastURL =
     "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&q=" +
     savedSearch +
     apiKey;
@@ -177,22 +177,22 @@ $.ajax({
     url: forecastURL,
     method: "GET",
   }).then(function (forecast) {
-    var forecasts = forecast.list;
+    let forecasts = forecast.list;
 
     $("#5dayForecast").empty();
-    for (var i = 0; i < forecasts.length; i++) {
-      var forecastDate = $("<p>").text(
+    for (let i = 0; i < forecasts.length; i++) {
+      let forecastDate = $("<p>").text(
         forecasts[i].dt_txt.replace("00:00:00", " ")
       );
-      var forecastCondition = $("<p>").text(forecasts[i].weather[0].main);
-      var forecastTemp = $("<p>").text("Temp: " + forecasts[i].main.temp);
-      var forecastHumidity = $("<p>").text(
+      let forecastCondition = $("<p>").text(forecasts[i].weather[0].main);
+      let forecastTemp = $("<p>").text("Temp: " + forecasts[i].main.temp);
+      let forecastHumidity = $("<p>").text(
         "Humidity: " + forecasts[i].main.humidity
       );
 
       //displays each day separately
       if ([i] % 8 === 0) {
-        var newDiv = $("<div>").attr("class", "col card mr-3 forecast");
+        let newDiv = $("<div>").attr("class", "col card mr-3 forecast");
         newDiv.append(
           forecastDate,
           forecastCondition,
